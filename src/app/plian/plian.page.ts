9cbbb25ba854c479366cbe39697ef33136ca793e
Ra-is13 <rais13122544@gmail.com>
2022-11-05 13:15:10 +0700

    rais

--- src/app/plian/plian.page.ts
+++ src/app/plian/plian.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-plian',
   templateUrl: './plian.page.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlianPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
   }
-
+gototab4(){
+  this.navCtrl.navigateForward('tabs/tab4');
+}
 }
